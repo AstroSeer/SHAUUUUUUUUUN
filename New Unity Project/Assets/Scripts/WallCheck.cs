60b261c006d6ccc3fe76228978fad646efdb93d9
AstroSeer <58754122+AstroSeer@users.noreply.github.com>
2021-11-11 18:01:26 -0500

    WIP Wall Fix

--- New Unity Project/Assets/Scripts/WallCheck.cs
+++ New Unity Project/Assets/Scripts/WallCheck.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraBehavior : MonoBehaviour
+public class WallCheck : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
