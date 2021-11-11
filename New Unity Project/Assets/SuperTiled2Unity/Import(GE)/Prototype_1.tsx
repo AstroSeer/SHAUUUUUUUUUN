8c07e0356bc6b11b33d903a755c3d849cf3b3444
Oliver Rayner <44844161+OliverRayner@users.noreply.github.com>
2021-11-11 11:15:31 -0500

    Unity Characters Look Nicer

--- New Unity Project/Assets/SuperTiled2Unity/Import(GE)/Prototype_1.tsx
+++ New Unity Project/Assets/SuperTiled2Unity/Import(GE)/Prototype_1.tsx
@@ -1,18 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="Prototype_1" tilewidth="16" tileheight="16" tilecount="30" columns="5">
  <image source="Prototype.png" width="80" height="96"/>
- <tile id="0">
-  <objectgroup draworder="index" id="2">
-   <object id="3" x="0" y="0" width="16" height="16"/>
-   <object id="4" x="8" y="8" width="1"/>
-   <object id="6" x="0" y="0.125" width="16" height="15.625"/>
-  </objectgroup>
- </tile>
- <tile id="13">
-  <objectgroup draworder="index" id="2">
-   <object id="2" x="0.875" y="4.625">
-    <polygon points="0,0 6.125,11.5 14.625,11.375 15.25,-4.5 -1,-4.625"/>
-   </object>
-  </objectgroup>
- </tile>
 </tileset>
