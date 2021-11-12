9f36993d71d3ae69de5a74e79eeeb2f1b517737c
Oliver Rayner <44844161+OliverRayner@users.noreply.github.com>
2021-11-12 16:46:07 -0500

    Tiled Collisions are Great

---
+++ New Unity Project/Assets/SuperTiled2Unity/Import(GE)/1st_Level.tsx
@@ -0,0 +1,135 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Prototype_1" tilewidth="16" tileheight="16" tilecount="30" columns="5">
+ <image source="1st_Level.png" width="80" height="96"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.4348" y="16.0435">
+    <polygon points="0,0 5.52174,-12.1739 5.65217,-0.130435"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="3.95652">
+    <polygon points="0,0 6.08696,11.913 -0.0869565,12"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.52174" y="15.9565">
+    <polygon points="0,0 7.65217,-16 13.3913,-15.913 13.4783,-0.0869565"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="2.26087">
+    <polygon points="0,0 -13.6522,13.6522 -0.0869565,13.6957"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="2.30435">
+    <polygon points="0,0 13.913,13.6957 -0.0434783,13.6957"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.30435" y="-0.0434783">
+    <polygon points="0,0 7.65217,16.087 13.7826,16 13.7826,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.34783" y="0.0434783">
+    <polygon points="0,0 13.6087,13.8696 13.6087,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="13.5217">
+    <polygon points="0,0 13.8696,-13.5217 0,-13.4783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.087" y="-0.0434783">
+    <polygon points="0,0 5.69565,12.0435 5.73913,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.3913" y="0.0869565">
+    <polygon points="0,0 -5.3913,11.6957 -5.43478,-0.130435"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.9565" y="9.95652">
+    <polygon points="0,0 -12.0435,6.08696 -0.130435,6.08696"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="1.82609">
+    <polygon points="0.0434783,0.913043 -15.9565,8.26087 -16.087,14.1739 -0.0434783,14.1739"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="2.43478">
+    <polygon points="0,0 16.0435,8 16,13.4348 0.173913,13.5652"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.130435" y="10.2174">
+    <polygon points="0,0 12.087,5.65217 -0.217391,5.82609"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="5.86957">
+    <polygon points="0,0 -12.087,-5.86957 -0.0434783,-5.86957"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="13.9565">
+    <polygon points="0,0 0,-13.9565 -16.0435,-13.9565 -16.0435,-7.04348"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0434783" y="13.4783">
+    <polygon points="0,0 16,-7.21739 16,-13.4783 0.0869565,-13.4783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="12.087" y="0.0434783">
+    <polygon points="0,0 -12.087,6.17391 -12.1304,-0.173913"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
--- New Unity Project/Assets/SuperTiled2Unity/Import(GE)/Prototype_1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Prototype_1" tilewidth="16" tileheight="16" tilecount="30" columns="5">
- <image source="Prototype.png" width="80" height="96"/>
-</tileset>
