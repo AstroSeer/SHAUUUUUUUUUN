08fcc3ed65132706a271d25dea0b8b06832b084c
Oliver Rayner <44844161+OliverRayner@users.noreply.github.com>
2021-11-20 17:51:44 -0500

    Better_Animation

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -67,10 +67,7 @@ public class PlayerMovement : MonoBehaviour {
     void Jump() {
         if(Input.GetButtonDown("Jump") && grounded == true) {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, playerJumpHeight), ForceMode2D.Impulse);
-            animator.SetBool("IsJumping", true);
-        }
-        if(grounded == false) {
-            animator.SetBool("IsJumping", false);
+            animator.SetTrigger("IsJumping");
         }
     }
 }
