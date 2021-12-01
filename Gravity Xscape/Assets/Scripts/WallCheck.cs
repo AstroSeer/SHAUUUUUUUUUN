28cf36b73479593448e645c42c32d2e4aa2f4e34
Oliver Rayner <44844161+OliverRayner@users.noreply.github.com>
2021-12-01 00:23:32 -0500

    Seems to be working

--- Gravity Xscape/Assets/Scripts/WallCheck.cs
+++ Gravity Xscape/Assets/Scripts/WallCheck.cs
@@ -14,24 +14,22 @@ public class WallCheck : MonoBehaviour {
 
     }
     private void OnCollisionEnter2D(Collision2D collision) {
-        float otherPos = collision.gameObject.transform.position.x;
-        float selfPos = Player.transform.position.x;
-        if(collision.collider.tag == "Wall") {
-            float diff = selfPos - otherPos;
-            //Debug.Log("otherPos is " + otherPos);
-            //Debug.Log("selfPos is " + selfPos);
-            //Debug.Log("diff is " + diff);
-            if (diff < 0) {
-                Player.GetComponent<PlayerMovement>().wallRight = true;
-            }
-            else {
-                Player.GetComponent<PlayerMovement>().wallLeft = true;
-            }
+        if (collision.collider.tag == "RightWall")
+        {
+            Player.GetComponent<PlayerMovement>().wallRight = true;
+        }
+        else if (collision.collider.tag == "LeftWall")
+        {
+            Player.GetComponent<PlayerMovement>().wallLeft = true;
         }
     }
     private void OnCollisionExit2D(Collision2D collision) {
-        if (collision.collider.tag == "Wall") {
+        if (collision.collider.tag == "RightWall")
+        {
             Player.GetComponent<PlayerMovement>().wallRight = false;
+        }
+        else if (collision.collider.tag == "LeftWall")
+        {
             Player.GetComponent<PlayerMovement>().wallLeft = false;
         }
     }
