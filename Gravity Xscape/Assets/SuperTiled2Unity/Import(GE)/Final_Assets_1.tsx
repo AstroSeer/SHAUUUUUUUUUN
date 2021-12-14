8158a50ababc5671a74028b8e2d29efaca1187b8
Oliver Rayner <44844161+OliverRayner@users.noreply.github.com>
2021-12-13 19:48:05 -0500

    Unity_Update

--- Gravity Xscape/Assets/SuperTiled2Unity/Import(GE)/Final_Assets_1.tsx
+++ Gravity Xscape/Assets/SuperTiled2Unity/Import(GE)/Final_Assets_1.tsx
@@ -1,6 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="Final_Assets_1" tilewidth="16" tileheight="16" tilecount="132" columns="12">
  <image source="Final_World_Assets_1.png" width="192" height="176"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="10" width="16" height="6"/>
+  </objectgroup>
+ </tile>
  <tile id="4">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="16"/>
