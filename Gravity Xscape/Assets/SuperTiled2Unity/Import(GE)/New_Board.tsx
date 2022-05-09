702d167ecc7bec57631e5869395a3bf63b713eb1
Oliver Rayner <44844161+OliverRayner@users.noreply.github.com>
2022-05-09 16:57:10 -0400

    Updated Game

---
+++ Gravity Xscape/Assets/SuperTiled2Unity/Import(GE)/New_Board.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="New_Board" tilewidth="16" tileheight="16" tilecount="12" columns="3">
+ <image source="Update_Pipe_Board 1.png" width="48" height="64"/>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="2" width="1" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15" y="2" width="1" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="2" width="16" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="2" width="16" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="2" width="16" height="8"/>
+  </objectgroup>
+ </tile>
+</tileset>
